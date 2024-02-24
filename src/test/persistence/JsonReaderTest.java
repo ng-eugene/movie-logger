aa091a0e02a758da6ee35898da39c7494e2a1530
Eugene Ng <eng04@students.cs.ubc.ca>
2024-02-24 00:51:16 -0800

    Add tests for JSON functionality

---
+++ src/test/persistence/JsonReaderTest.java
@@ -0,0 +1,65 @@
+package persistence;
+
+import model.MovieList;
+import model.MovieLog;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonReaderTest {
+
+    private JsonReader reader;
+    private MovieList list;
+    private MovieLog log;
+
+    @BeforeEach
+    public void setup() {
+        reader = new JsonReader("./data/testread.json");
+        list = new MovieList();
+        log = new MovieLog();
+    }
+
+    @Test
+    public void readerTest() {
+        try {
+            reader.read(list, log);
+            assertEquals(2, list.getNumMovies());
+            assertEquals(2, log.getNumMovies());
+            assertEquals("The Departed", list.getMovie(0).getName());
+            assertEquals("Good Will Hunting", log.getMovie(0).getName());
+            assertEquals("William", log.getMovie(0).getReview());
+            assertEquals("2024-01-15", log.getMovie(0).dateWatched());
+            assertEquals(9.235, log.getMovie(0).getRating());
+            assertTrue(log.getMovie(0).isRewatchable());
+        } catch (IOException e) {
+            fail("Unexpected IOException");
+        }
+    }
+
+    @Test
+    public void invalidFileTest() {
+        try {
+            reader = new JsonReader("./data/null.json");
+            reader.read(list, log);
+            fail("IOException was not thrown");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    public void emptyFileTest() {
+        try {
+            reader = new JsonReader("./data/testreadempty.json");
+            reader.read(list, log);
+            assertEquals(0, list.getNumMovies());
+            assertEquals(0, log.getNumMovies());
+        } catch (IOException e) {
+            fail("Unexpected IOException");
+        }
+    }
+
+}
